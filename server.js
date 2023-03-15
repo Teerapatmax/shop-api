35cabe37fc69b9a9fe1a4df4f70a8e6c11b9a5ad
Teerapat prasongprasit <Teerapat13608@gmail.com>
2023-03-15 17:22:18 +0700

    update

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get('/product', function(req, res) {
   )
 })
 // เรียงลับดับจากคนที่ซื้อเยอะ => น้อยที่สุด
-app.get("/top_customer", function (req, res) {
+app.get("/top_customers", function (req, res) {
   connection.query(
     `SELECT 
     C.firstName, 
@@ -70,7 +70,7 @@ app.get("/top_customer", function (req, res) {
 });
 
 // เรียงลับดับจากคนที่ซื้อเยอะ => น้อยที่สุด
-app.get('/top_product', function(req, res){
+app.get('/top_products', function(req, res){
   connection.query(
     `SELECT O.id, P.PName, O.QTY, SUM(O.QTY*P.price) as Total_QTY FROM a1_order as O INNER JOIN a1_product as P ON O.Pid = P.Pid GROUP BY O.id, P.PName, O.QTY, P.price ORDER BY Total_QTY DESC;`,
     function (err, results) {
